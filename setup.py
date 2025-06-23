23a1d954dbc5ffacf282240d3f02895c80dc1298
shubham968 <maakarshubham@gmail.com>
2025-06-23 16:55:39 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'mlproject',
+    version = 0.0.1,
+    author='shubham',
+    author_email = 'maakarshubham@gmail.com',
+    packages = find_packages()
+    install_requires = ['pandas','numpy','seaborn','matplotlib.pyplot']
+
+
+
+)
